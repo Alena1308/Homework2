e452b65c6300ef6e242638ea51c6422d110e7000
Alena1308 <Lenamironova13@mail.ru>
2022-09-21 16:59:00 +0300

    The third commit

--- src/Main.java
+++ src/Main.java
@@ -9,44 +9,47 @@ public class Main {
         System.out.println(paper);
 //задание 2
         dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
+        cat += 4;
+        paper += 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 //задание 3
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 //задание 4
         var friend = 19;
         System.out.println(friend);
-        friend = friend + 2;
+        friend += 2;
         System.out.println(friend);
-        friend = friend / 7;
+        friend /= 7;
         System.out.println(friend);
 //задание 5
         var frog = 3.5;
         System.out.println(frog);
-        frog = frog * 10;
+        frog *= 10;
         System.out.println(frog);
-        frog = frog / 3.5;
+        frog /= 3.5;
         System.out.println(frog);
-        frog = frog + 4;
+        frog += 4;
         System.out.println(frog);
 //задание 6
         var weightFirstFighter = 78.2;
         var weightSecondFighter = 82.7;
         var totalWeightTwoFighters = weightFirstFighter + weightSecondFighter;
         System.out.println("Общий вес двух бойцов " + totalWeightTwoFighters + " кг");
-        var differenceWeightTwoFighters = weightSecondFighter % weightFirstFighter;
+        var differenceWeightTwoFighters = weightFirstFighter - weightSecondFighter;
         System.out.println("Разница в весе двух бойцов " + differenceWeightTwoFighters + " кг");
 //задание 7
-        var differenceWeightTwoFighters2 = weightSecondFighter - weightFirstFighter;
+        var differenceWeightTwoFighters2 = weightSecondFighter % weightFirstFighter;
         System.out.println("Разница в весе двух бойцов " + differenceWeightTwoFighters2 + " кг");
+        var differenceWeightTwoFighters3 = weightSecondFighter - weightFirstFighter;
+        System.out.println("Разница в весе двух бойцов " + differenceWeightTwoFighters3 + " кг");
+        //есть вариант с выведением модуля, но он не решит проблему выбора большего значения с помощью кода
 //задание 8
         var totalHours = 640;
         var oneEmployeeHours = 8;
